6a8725a21811eabacbe9105832166baf33737748
AmilaBandara1994 <amilaprasadbandara1994@gmail.com>
2024-08-13 15:08:01 +0530

    hero section and popular product section completed

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import {CustomerReviews , Footer, Hero, SupperQuality, 
+  SpecialOffers, Subscribe, PopularProducts, Services
+} from './sections'
+import Nav from './components/nav'
+
+
+const App = () =>  (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SupperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <SpecialOffers /> 
+      </section>
+      <section className="padding">
+        <Services />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  )
+export default App
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SupperQuality from './SupperQuality';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Subscribe from './Subscribe';
+
+export{
+    Hero,
+    PopularProducts,
+    SupperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReviews,
+    Footer,
+    Subscribe
+}
